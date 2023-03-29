f4cae6bd27721cec06bcfbf6077f64e0c3627eee
Yurii Shevchenko <102354607+yshvchnk@users.noreply.github.com>
2023-03-29 14:47:35 +0300

    React.js-MaterialUI-KeeperNotesApp

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    const date = new Date();
+    const year = date.getFullYear();
+    return (
+        <footer>
+            <p>Copyright &copy; {year}</p>
+        </footer>
+    );
+}
+
+export default Footer;
